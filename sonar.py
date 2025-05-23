be6bc491d343d24ab9f877968fc972060308bb54
Guilherme Zeus Dantas e Moura <zeusdanmou@gmail.com>
2025-05-23 15:21:54 -0400

    Change log file name from arxiv_notifier.log to sonar.log

--- sonar.py
+++ sonar.py
@@ -14,7 +14,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
     handlers=[
-        logging.FileHandler("arxiv_notifier.log"),
+        logging.FileHandler("sonar.log"),
         logging.StreamHandler()
     ]
 )
